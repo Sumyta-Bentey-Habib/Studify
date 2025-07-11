88cac74c01e8a31fb182b957ae5fbb1d54800595
Your Name <you@example.com>
2025-07-11 20:57:07 +0600

    structure

---
+++ src/router/router.jsx
@@ -0,0 +1,20 @@
+import { createBrowserRouter } from "react-router";
+import HomeLayout from "../layouts/HomeLayout";
+import Home from "../pages/Home";
+
+export const router = createBrowserRouter([
+    {
+        path:"/",
+        element:<HomeLayout></HomeLayout>,
+        children:[
+            {
+                index:true,
+                element:<Home></Home>
+
+            },
+
+        ]
+    },
+    
+]
+);
